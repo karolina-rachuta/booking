fc04f746cbd917fe604fe359348760d6309931c8
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-01 18:10:50 +0400

    creating routes and models for Hotels

---
+++ api/routes/hotels.js
@@ -0,0 +1,28 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", async (req, res) => {
+
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+
+    } catch (error) {
+        res.status(500).json(err);
+    }
+})
+
+//UPDATE
+
+//DELETE
+
+//GET
+
+//GET A HOTEL
+
+export default router;
