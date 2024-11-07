a75f3d13c47cfd6b71d331f5319aefeb6cb78f82
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-07 17:56:09 +0400

    jwt token, cookie parser, and creating function to verify user from its token and verifyAdmin ect

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -24,6 +25,7 @@ mongoose.connection.on("disconnected", () => {
 
 //middlewares -
 app.use(express.json());
+app.use(cookieParser());
 
 // when you go to endpoint auth, you will use authRoute
 app.use("/api/auth", authRoute); // http://localhost:8800/api/auth and http://localhost:8800/api/auth/register
