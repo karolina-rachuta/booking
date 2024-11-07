61a7fc6253b2e1d0029ca81e25bafc030eb14772
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-07 13:06:35 +0400

    handling routes CRUD of hotels and handling errors, creating err middleware and function

--- api/index.js
+++ api/index.js
@@ -22,13 +22,27 @@ mongoose.connection.on("disconnected", () => {
     console.log("mongoDB disconnected!")
 })
 
-//middlewares
+//middlewares -
 app.use(express.json());
+
 // when you go to endpoint auth, you will use authRoute
 app.use("/api/auth", authRoute); // http://localhost:8800/api/auth and http://localhost:8800/api/auth/register
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+})
+
 
 app.listen(8800, () => {
     connect();
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -20,10 +20,57 @@ router.post("/", async (req, res) => {
 
 //UPDATE
 
+router.put("/:id", async (req, res) => {
+    // mongoDb $set method to update hotel
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true});
+        // it will return new version of our document in postman
+        res.status(200).json(updatedHotel);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 //DELETE
+
+router.delete("/:id", async (req, res) => {
+    // mongoDb $set method to update hotel
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        // it will return new version of our document in postman
+        res.status(200).json("Hotel has been deleted.");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //GET
 
+router.get("/:id", async (req, res) => {
+    // mongoDb $set method to update hotel
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        // it will return new version of our document in postman
+        res.status(200).json(hotel);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 //GET A HOTEL
+
+router.get("/", async (req, res, next) => {
+
+    try {
+        const hotels = await Hotel.find();
+        // it will return new version of our document in postman
+        res.status(200).json(hotels);
+    } catch (err) {
+        next(err);
+        // res.status(500).json(err);
+    }
+});
 
 export default router;
