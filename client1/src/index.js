b67b2a3b46981f882358359c304ebeec2d3fd1be
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-12 20:19:29 +0400

    creating authContext

--- client1/src/index.js
+++ client1/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {SearchContextProvider} from '../src/context/SearchContext';
+import {AuthContextProvider} from '../src/context/AuthContext';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SearchContextProvider>
-      <App />
+        <AuthContextProvider>
+            <App />
+        </AuthContextProvider>
     </SearchContextProvider>
   </React.StrictMode>
 );
---
+++ client1/src/pages/login/Login.jsx
@@ -0,0 +1,59 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import "./login.css";
+
+function Login() {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("auth/login", credentials);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      navigate("/");
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE", payload: error.response.data });
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainter">
+        <input
+          className="lInput"
+          type="text"
+          id="username"
+          placeholder="username"
+          onChange={handleChange}
+        />
+        <input
+          className="lInput"
+          type="password"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button disables={loading} className="lButton" onClick={handleClick}>
+          Login
+        </button>
+        {error && <span>{error.message}</span>}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
