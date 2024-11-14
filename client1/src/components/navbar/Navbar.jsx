969b8efc07ce88009aaa1d0e08911239deba5131
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-14 10:27:22 +0400

    showing buttons to login or register if there is no user saved in local storage

--- client1/src/components/navbar/Navbar.jsx
+++ client1/src/components/navbar/Navbar.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
-import "./navbar.css"
+import "./navbar.css";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="navContainer">
-       <Link to="/"><span className="logo">KaroBooking</span></Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        <Link to="/">
+          <span className="logo">KaroBooking</span>
+        </Link>
+
+        {user ?  user.username : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
