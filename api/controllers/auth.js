f1da6810e0ac4ada69d0e060a2928213d1721cfe
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-19 18:43:25 +0400

    creating protected route, styling and structure and login of login component, creating AuthContext

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -38,7 +38,7 @@ export const login = async (req, res, next) => {
 
         //to not show password or isAdmin - because then it can be changed:
         const {password, isAdmin, ...otherDetails} = user._doc;
-        res.cookie("access_token", token, {httpOnly:true}).status(200).json({...otherDetails});
+        res.cookie("access_token", token, {httpOnly:true}).status(200).json({details: {...otherDetails}, isAdmin});
 
     } catch (err) {
         next(err);
