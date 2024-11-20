e53196c6f1a9e0b274770de4c625e9dfb453f663
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-11-20 12:04:59 +0400

    creating new users, fetching users data, display data in the table

--- admin/src/datatablesource.js
+++ admin/src/datatablesource.js
@@ -7,7 +7,7 @@ export const userColumns = [
     renderCell: (params) => {
       return (
         <div className="cellWithImg">
-          <img className="cellImg" src={params.row.img} alt="avatar" />
+          <img className="cellImg" src={params.row.img || "https://www.shutterstock.com/shutterstock/photos/1153673752/display_1500/stock-vector-profile-placeholder-image-gray-silhouette-no-photo-1153673752.jpg"} alt="avatar" />
           {params.row.username}
         </div>
       );
@@ -20,22 +20,20 @@ export const userColumns = [
   },
 
   {
-    field: "age",
-    headerName: "Age",
+    field: "country",
+    headerName: "Country",
     width: 100,
   },
   {
-    field: "status",
-    headerName: "Status",
-    width: 160,
-    renderCell: (params) => {
-      return (
-        <div className={`cellWithStatus ${params.row.status}`}>
-          {params.row.status}
-        </div>
-      );
-    },
+    field: "city",
+    headerName: "City",
+    width: 100,
   },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 100,
+  }
 ];
 
 //temporary data
